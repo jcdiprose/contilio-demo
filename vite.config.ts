9be2c6c336dad02a9a85e2f4a7d1f53e228bbd3c
jcdiprose <calumdiprose@outlook.com>
2024-07-29 22:20:28 +0100

    add vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite"
 import { fileURLToPath } from "url"
 import { resolve, dirname } from "path"
+import { viteStaticCopy } from "vite-plugin-static-copy"
+
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
@@ -10,4 +12,14 @@ export default defineConfig({
       src: resolve(__dirname, "src"),
     },
   },
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: "assets/*",
+          dest: "assets",
+        },
+      ],
+    }),
+  ],
 })
